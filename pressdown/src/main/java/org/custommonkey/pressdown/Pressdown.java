fb1afea83b57ccb04ba5c6cf579fb9e7f1b44148
Jeff Martin <jeff@custommonkey.org>
2013-11-23 23:39:42 +0000

    Added ReadableFile to manage input files

--- pressdown/src/main/java/org/custommonkey/pressdown/Pressdown.java
+++ pressdown/src/main/java/org/custommonkey/pressdown/Pressdown.java
@@ -1,6 +1,7 @@
 package org.custommonkey.pressdown;
 
 import static java.lang.System.err;
+import static org.custommonkey.pressdown.ReadableFile.readableFile;
 
 import java.io.*;
 import java.net.URL;
@@ -24,13 +25,13 @@ public class Pressdown {
         try {
 
             if (args.length == 0) {
-                new Pressdown().transform(file("README.md"));
+                new Pressdown().transform(readableFile("README.md"));
             } else {
 
-                final Pressdown app = new Pressdown(file(args[0]));
+                final Pressdown app = new Pressdown(readableFile(args[0]));
 
                 for (int i = 1; i < args.length; i++) {
-                    app.transform(file(args[i]));
+                    app.transform(readableFile(args[i]));
                 }
             }
 
@@ -40,28 +41,21 @@ public class Pressdown {
 
     }
 
-    private static String file(String file) throws FileNotFoundException {
-        if(!new File(file).exists()) {
-            throw new FileNotFoundException(file);
-        }
-        return file;
-    }
-
     private final Transformer transformer;
 
     Pressdown() throws TransformerConfigurationException, IOException {
         this(internalXSL());
     }
 
-    Pressdown(final String xsl) throws TransformerConfigurationException,
+    Pressdown(final ReadableFile xslFile) throws TransformerConfigurationException,
             IOException {
-        this(new StreamSource(xsl));
+        this(xslFile.asStreamSource());
     }
 
     Pressdown(final StreamSource xsl) throws TransformerConfigurationException,
             IOException {
         transformer = new TransformerFactoryImpl().newTransformer(xsl);
-        transformer.setParameter("css", read(resource("style.css")));
+        transformer.setParameter("css", read(resourceAsURL("style.css")));
     }
 
     private String read(final URL url) throws IOException {
@@ -72,27 +66,27 @@ public class Pressdown {
     }
 
     private static StreamSource internalXSL() throws IOException {
-        return new StreamSource(resource("pressdown.xsl").openStream());
+        return new StreamSource(resourceAsURL("pressdown.xsl").openStream());
     }
 
-    private static URL resource(final String name) {
+    private static URL resourceAsURL(final String name) {
         final URL url = Pressdown.class.getClassLoader().getResource(name);
         err.println(url);
         return url;
     }
 
-    public void transform(final String inputFile) throws TransformerException {
+    public void transform(final ReadableFile inputFile) throws TransformerException {
         final StreamResult outputFile = outputFile(inputFile);
         err.printf("%s -> %s%n", inputFile, outputFile.getSystemId());
         transformer.transform(markdownSource(inputFile), outputFile);
     }
 
-    private static StreamResult outputFile(final String args) {
-        return new StreamResult(args + ".html");
+    private static StreamResult outputFile(final ReadableFile inputFile) {
+        return new StreamResult(inputFile.getName() + ".html");
     }
 
-    private static SAXSource markdownSource(final String args) {
-        return new SAXSource(new SAXEventVisitor(), new InputSource(args));
+    private static SAXSource markdownSource(final ReadableFile inputFile) {
+        return new SAXSource(new SAXEventVisitor(), inputFile.asInputSource());
     }
 
     static char[] readChars(final InputSource in) throws IOException {
---
+++ pressdown/src/main/java/org/custommonkey/pressdown/ReadableFile.java
@@ -0,0 +1,33 @@
+package org.custommonkey.pressdown;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+
+import javax.xml.transform.stream.StreamSource;
+
+import org.xml.sax.InputSource;
+
+class ReadableFile extends File {
+
+	private static final long serialVersionUID = -4783309504397419263L;
+
+	static ReadableFile readableFile(String name) throws FileNotFoundException {
+		final ReadableFile file = new ReadableFile(name);
+		if (!file.canRead()) {
+			throw new FileNotFoundException(name);
+		}
+		return file;
+	}
+
+	private ReadableFile(String name) {
+		super(name);
+	}
+
+	StreamSource asStreamSource() {
+		return new StreamSource(this);
+	}
+
+	InputSource asInputSource() {
+		return new InputSource(this.getName());
+	}
+}
